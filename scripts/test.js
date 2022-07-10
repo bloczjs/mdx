97627c3b697c84fa00ea598c409c5d8d622f2a1a
Ayc0 <ayc0.benj@gmail.com>
2022-07-11 01:59:10 +0200

    #30@dev: add tests for rollup

--- scripts/test.js
+++ scripts/test.js
@@ -6,6 +6,7 @@ test("@blocz/mdx-plugin-detect-imports");
 test("@blocz/mdx-live");
 test("@blocz/webpack-test");
 test("@blocz/esbuild-test");
+test("@blocz/rollup-test");
 
 function test(package) {
     cp.spawnSync("yarn", ["workspace", package, "test"], {
---
+++ tests/rollup-test/rollup.test.mjs
@@ -0,0 +1,62 @@
+import test from "ava";
+
+import { rollup } from "rollup";
+import mdx from "@mdx-js/rollup";
+
+import detectImportsPlugin from "@blocz/mdx-plugin-detect-imports";
+
+const buildEntry = async (mdxOptions) => {
+    const inputOptions = {
+        input: "src/entry.mdx",
+        plugins: [mdx(mdxOptions)],
+    };
+
+    const outputOptions = {
+        file: "out.js",
+    };
+
+    let bundle;
+    try {
+        bundle = await rollup(inputOptions);
+        const { output } = await bundle.generate(outputOptions);
+        return output[0].code;
+    } catch (error) {
+        throw error;
+    } finally {
+        if (bundle) {
+            await bundle.close();
+        }
+    }
+};
+
+test("it works with rollup", async (t) => {
+    const content = await buildEntry({
+        remarkPlugins: [detectImportsPlugin],
+    });
+
+    t.truthy(content.match(/\.h2, {\s*children: "Hello MDX"/));
+    t.truthy(content.match(/\.li, {\s*children: "First item"/));
+    t.truthy(content.match(/\.li, {\s*children: "Second item"/));
+    t.truthy(content.match(/Button, {\s*variant: "blue",\s*label: "Label"/));
+    t.truthy(
+        content.includes(
+            `const importStatements = [{ module: "./elements", imports: [{ kind: "named", local: "Tabs", value: Tabs }, { kind: "named", local: "Button", value: Button }] }];`,
+        ),
+    );
+});
+
+test("it works with rollup and a custom name", async (t) => {
+    const content = await buildEntry({
+        remarkPlugins: [[detectImportsPlugin, { exportName: "otherName" }]],
+    });
+
+    t.truthy(content.match(/\.h2, {\s*children: "Hello MDX"/));
+    t.truthy(content.match(/\.li, {\s*children: "First item"/));
+    t.truthy(content.match(/\.li, {\s*children: "Second item"/));
+    t.truthy(content.match(/Button, {\s*variant: "blue",\s*label: "Label"/));
+    t.truthy(
+        content.includes(
+            `const otherName = [{ module: "./elements", imports: [{ kind: "named", local: "Tabs", value: Tabs }, { kind: "named", local: "Button", value: Button }] }];`,
+        ),
+    );
+});
