085a0f0a863e52a12a267fb314fa172c4bbf2119
Ayc0 <ayc0.benj@gmail.com>
2022-07-10 15:11:15 +0200

    [webpack] add test for require.resolve

--- packages/webpack-test/webpack.test.js
+++ packages/webpack-test/webpack.test.js
@@ -65,3 +65,66 @@ test("it works with webpack", (t) => {
         });
     });
 });
+
+test("it works with webpack and require.resolve", (t) => {
+    const fs = createFsFromVolume(new Volume());
+
+    const compiler = webpack({
+        entry: "./src/entry.mdx",
+        output: {
+            filename: "out.js",
+            path: "/",
+        },
+        target: "web",
+        mode: "production",
+        // Disable tree shaking
+        optimization: {
+            usedExports: false,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mdx?$/,
+                    use: [
+                        {
+                            loader: require.resolve("@mdx-js/loader"),
+                            options: {
+                                remarkPlugins: [detectImportsPlugin],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    });
+    compiler.outputFileSystem = fs;
+
+    return new Promise((res, rej) => {
+        compiler.run((err) => {
+            if (err) {
+                rej(err);
+                return;
+            }
+            const content = fs.readFileSync("/out.js", "utf-8");
+            compiler.close((closeErr) => {
+                if (closeErr) {
+                    rej(closeErr);
+                    return;
+                }
+                t.truthy(content.includes('.h2,{children:"Hello MDX"})'));
+                t.truthy(content.includes('.li,{children:"First item"})'));
+                t.truthy(content.includes('.li,{children:"Second item"})'));
+                t.truthy(
+                    content.includes('.Button,{variant:"blue",label:"Label"})'),
+                );
+                t.truthy(content.includes("importStatements"));
+                t.truthy(
+                    content.includes(
+                        '[{module:"./elements",imports:[{kind:"named",local:"Tabs",value:t.Tabs},{kind:"named",local:"Button",value:t.Button}]}]',
+                    ),
+                );
+                res();
+            });
+        });
+    });
+});
