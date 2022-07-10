1d2f0d128f533c6c8ba8bebf7a16533ada49c038
Ayc0 <ayc0.benj@gmail.com>
2022-07-10 15:13:33 +0200

    Add more tests

--- packages/mdx-js-test/mdx.test.mjs
+++ packages/mdx-js-test/mdx.test.mjs
@@ -23,3 +23,24 @@ test("it works with @mdx-js/mdx", async (t) => {
         ),
     );
 });
+
+test("it works with @mdx-js/mdx and a custom name", async (t) => {
+    const file = `import { Tabs, Button } from "./elements";
+
+## Hello MDX
+
+1. First item
+2. Second item
+
+<Button variant="blue" label="Label" />`;
+
+    const vFile = await compile(file, {
+        remarkPlugins: [[detectImportsPlugin, { exportName: "otherName" }]],
+    });
+
+    t.truthy(
+        vFile.value.includes(
+            'export const otherName = [{ module: "./elements", imports: [{ kind: "named", local: "Tabs", value: Tabs }, { kind: "named", local: "Button", value: Button }] }];\n',
+        ),
+    );
+});
--- packages/webpack-test/webpack.test.js
+++ packages/webpack-test/webpack.test.js
@@ -66,6 +66,74 @@ test("it works with webpack", (t) => {
     });
 });
 
+test("it works with webpack and a custom name", (t) => {
+    const fs = createFsFromVolume(new Volume());
+
+    const compiler = webpack({
+        entry: "./src/entry.mdx",
+        output: {
+            filename: "out.js",
+            path: "/",
+        },
+        target: "web",
+        mode: "production",
+        // Disable tree shaking
+        optimization: {
+            usedExports: false,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mdx?$/,
+                    use: [
+                        {
+                            loader: "@mdx-js/loader",
+                            options: {
+                                remarkPlugins: [
+                                    [
+                                        detectImportsPlugin,
+                                        { exportName: "otherName" },
+                                    ],
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    });
+    compiler.outputFileSystem = fs;
+
+    return new Promise((res, rej) => {
+        compiler.run((err) => {
+            if (err) {
+                rej(err);
+                return;
+            }
+            const content = fs.readFileSync("/out.js", "utf-8");
+            compiler.close((closeErr) => {
+                if (closeErr) {
+                    rej(closeErr);
+                    return;
+                }
+                t.truthy(content.includes('.h2,{children:"Hello MDX"})'));
+                t.truthy(content.includes('.li,{children:"First item"})'));
+                t.truthy(content.includes('.li,{children:"Second item"})'));
+                t.truthy(
+                    content.includes('.Button,{variant:"blue",label:"Label"})'),
+                );
+                t.truthy(content.includes("otherName"));
+                t.truthy(
+                    content.includes(
+                        '[{module:"./elements",imports:[{kind:"named",local:"Tabs",value:t.Tabs},{kind:"named",local:"Button",value:t.Button}]}]',
+                    ),
+                );
+                res();
+            });
+        });
+    });
+});
+
 test("it works with webpack and require.resolve", (t) => {
     const fs = createFsFromVolume(new Volume());
 
